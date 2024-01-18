cd1f6f38780d6e144c81d3c7031034b45a3cf7dc
Juan Villa <ing.villa.jimenez@gmail.com>
2024-01-18 15:37:46 -0600

    Before We Start Coding: Debugging

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
+  //////////////////////////////////////////////
+  // Before We Start Coding: Debugging
+  const x = "jonas";
+
   return <h1>Hello React!</h1>;
 }
 
 // React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
+//////////////////////////////////////////////
+// Rendering the Root Component and Strict Mode
 root.render(
   <React.StrictMode>
     <App />
@@ -14,4 +20,4 @@ root.render(
 );
 
 // React before 18
-// ReactDOM.render(<App />);
+// ReactDOM.render(<App />, document.getElementById("root"));
