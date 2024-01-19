f505a3d6d4b7c3a1b751324a7a92068ab3962775
Juan Villa <ing.villa.jimenez@gmail.com>
2024-01-19 11:33:33 -0600

    Destructuring Props

--- src/index.js
+++ src/index.js
@@ -126,20 +126,22 @@ function Menu() {
 
 //////////////////////////////////////////////
 // Creating And Reusing a Component
-function Pizza(props) {
-  console.log(props);
+//////////////////////////////////////////////
+// Destructuring Props
+function Pizza({ pizzaObj }) {
+  console.log(pizzaObj);
 
   //////////////////////////////////////////////
   // Conditional Rendering With Multiple Returns
-  if (props.pizzaObj.soldOut) return null;
+  if (pizzaObj.soldOut) return null;
 
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price}</span>
       </div>
     </li>
   );
@@ -176,7 +178,7 @@ function Footer() {
       )} */}
 
       {isOpen ? (
-        <Order closeHour={closeHour} />
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00
@@ -190,11 +192,14 @@ function Footer() {
 
 //////////////////////////////////////////////
 // Extracting JSX Into a New Component
-function Order(props) {
+//////////////////////////////////////////////
+// Destructuring Props
+function Order({ closeHour, openHour }) {
   return (
     <div className="order">
       <p>
-        We're open until {props.closeHour}:00. Come visit us or order online.
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
       </p>
       <button className="btn">Order</button>
     </div>
