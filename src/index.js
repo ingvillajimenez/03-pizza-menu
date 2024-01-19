c4f45ff4345f87397e18e5f7e57d926f5a2cc2fa
Juan Villa <ing.villa.jimenez@gmail.com>
2024-01-19 11:56:13 -0600

    React Fragments

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ function Header() {
   //////////////////////////////////////////////
   // Styling React Applications
   return (
-    <header className="header footer">
+    <header className="header">
       <h1 style={style}>Fast React Pizza Co.</h1>
     </header>
   );
@@ -87,9 +87,12 @@ function Menu() {
   // Rendering Lists
   //////////////////////////////////////////////
   // Conditional Rendering With Ternaries
+  //////////////////////////////////////////////
+  // React Fragments
   return (
     <main className="menu">
       <h2>Our menu</h2>
+
       {/* {numPizzas > 0 && (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
@@ -99,11 +102,18 @@ function Menu() {
       )} */}
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later :)</p>
       )}
